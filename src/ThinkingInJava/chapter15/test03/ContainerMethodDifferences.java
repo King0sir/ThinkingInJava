0d9f0b9a0d067ef4809dd8eeb6c7cbdf7a7c424a
king0sir <king0sir@163.com>
2016-02-01 19:21:56 +0800

    20160201company

---
+++ src/ThinkingInJava/chapter15/test03/ContainerMethodDifferences.java
@@ -0,0 +1,58 @@
+package ThinkingInJava.chapter15.test03;
+
+import java.lang.reflect.Method;
+import java.util.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ipaynow
+ * Date: 16-2-1
+ * Time: 下午7:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class ContainerMethodDifferences  {
+    static Set<String > methodSet(Class<?> type){
+        Set<String> result = new TreeSet<String>();
+        for(Method m:type.getMethods()){
+            result.add(m.getName());
+        }
+        return result;
+    }
+
+    static void interfaces(Class<?> type){
+        System.out.println("Interfaces in "+type.getSimpleName()+": ");
+        List<String > result = new ArrayList<String>();
+        for(Class<?> c: type.getInterfaces()){
+            result.add(c.getSimpleName());
+        }
+        System.out.println(result);
+    }
+    static Set<String> object = methodSet(Object.class);
+    static{object.add("clone");}
+    static void difference(Class<?> superset, Class<?> subset){
+        System.out.println(superset.getSimpleName()+" extends "+subset.getSimpleName()+", adds: ");
+        Set<String> comp = Sets.difference(methodSet(superset),methodSet(subset));
+        comp.removeAll(object);
+        interfaces(superset);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Collection: " +methodSet(Collection.class));
+        interfaces(Collection.class);
+        difference(Set.class,Collection.class);
+        difference(HashSet.class,Set.class);
+        difference(LinkedHashSet.class,HashSet.class);
+        difference(TreeSet.class,Set.class);
+        difference(List.class,Collection.class);
+        difference(ArrayList.class,List.class);
+        difference(LinkedList.class,List.class);
+        difference(Queue.class,Collection.class);
+        difference(PriorityQueue.class,Queue.class);
+        System.out.println("Map: "+methodSet(Map.class));
+        difference(HashMap.class,Map.class);
+        difference(LinkedHashMap.class,HashMap.class);
+        difference(SortedMap.class,Map.class);
+        difference(TreeMap.class,Map.class);
+
+    }
+}
